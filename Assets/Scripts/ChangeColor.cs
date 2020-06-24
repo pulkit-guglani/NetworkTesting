878420c83307d4365fcfcdb1a00b9e55ac164dc9
pulkit1011 <pulkitname@gmail.com>
2020-06-24 18:53:24 +0530

    color changes with deopdown inplemented

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColor : MonoBehaviour
+{
+    private Color[] _colors;
+    
+    public Dropdown Color;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _colors = new[] {UnityEngine.Color.red,UnityEngine.Color.green, UnityEngine.Color.blue, UnityEngine.Color.yellow};
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetColor()
+    {
+        Camera.main.backgroundColor = _colors[Color.value];
+    }
+    
+}
