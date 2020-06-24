9209132e7e055ea7cc860bf5a7f72b311394ed5a
Crimson <siddharthmishraaaaa@gmail.com>
2020-06-24 20:25:15 +0530

    turn pe kaam

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -4,18 +4,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ChangeColor : MonoBehaviour
+public class ColorChanger : MonoBehaviour
 {
     private Color[] _colors;
     
     public Dropdown Color;
-    // Start is called before the first frame update
     void Start()
     {
         _colors = new[] {UnityEngine.Color.red,UnityEngine.Color.green, UnityEngine.Color.blue, UnityEngine.Color.yellow};
     }
 
-    // Update is called once per frame
     void Update()
     {
         
---
+++ Assets/TurnManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using SWNetwork;
+
+public class TurnManager : MonoBehaviour
+{
+    private SyncPropertyAgent syncAgent;
+    private NetworkID nID;
+
+    private const string TURN = "Turn";
+
+    private void Start()
+    {
+        syncAgent = GetComponent<SyncPropertyAgent>();
+        nID = GetComponent<NetworkID>();
+    }
+
+    public void ChangeTurn()
+    {
+        if(syncAgent.GetPropertyWithName(TURN).GetIntValue() == 1)
+        {
+            syncAgent.Modify(TURN, 2);
+        }
+        else
+        {
+            syncAgent.Modify(TURN, 1);
+        }
+    }
+}
