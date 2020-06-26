289abecb84671da3f23ed08d469cc908a6e6eb34
Crimson <siddharthmishraaaaa@gmail.com>
2020-06-26 18:33:41 +0530

    wrkin

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -16,7 +16,7 @@ public class TurnManager : MonoBehaviour
 
     public void ChangeTurn()
     {
-        int currentTurn = syncAgent.GetPropertyWithName(TURN).GetIntValue();
+        int currentTurn = syncAgent.GetPropertyWithName("Turn").GetIntValue();
         if(currentTurn == 1)
         {
             syncAgent.Modify(TURN, 2);
@@ -25,14 +25,14 @@ public class TurnManager : MonoBehaviour
         {
             syncAgent.Modify(TURN, 1);
         }
-        Debug.Log("turn online vala hai : " + syncAgent.GetPropertyWithName(TURN).GetIntValue());
+        Debug.Log("turn online vala hai : " + syncAgent.GetPropertyWithName("Turn").GetIntValue());
 
       //  Debug.Log("abhi baari iski hai: "+currentTurn);
     }
 
     public void debugKr()
     {
-        int currentTurn = syncAgent.GetPropertyWithName(TURN).GetIntValue();
+        int currentTurn = syncAgent.GetPropertyWithName("Turn").GetIntValue();
         Debug.Log("abhi baari iski hai: "+currentTurn);
     }
     
